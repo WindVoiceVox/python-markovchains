969d70488327c8ac8ba40075eddc61a3ed7144ab
WindVoiceVox <windvoice.vox@gmail.com>
2016-07-23 21:01:06 +0900

    mysql/postgresqlは必要になってからimport

--- markovchains/database.py
+++ markovchains/database.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import mysql
-import postgresql
 
 class Database(object):
 
     @classmethod
     def create(cls, db, dbname):
         if db == 'mysql':
+            import mysql
             return mysql.MySQL(dbname)
         elif db == 'postgresql':
+            import postgresql
             return postgresql.PostgreSQL(dbname)
 
